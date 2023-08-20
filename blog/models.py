a08bfead4e6f15c24f8770afcb3cd157d97eae04
Strasman <elad.strasman@gmail.com>
2023-08-20 17:14:38 +0300

    ORM basic

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,16 @@ class Author(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_logged_in = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name + " : " + self.email
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
@@ -22,12 +27,17 @@ class Tag(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     content = models.TextField()
-    publication_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
