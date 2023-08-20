d616f302ef8adae47323da781bacd94bac656c10
Strasman <elad.strasman@gmail.com>
2023-08-20 13:10:53 +0300

    Templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
+from django import template
 import datetime
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello Django")
 
 def today_is(request):
     now = datetime.datetime.now()
-    html = "<html><body>Current date and time: {0}</body></html>".format(now)
+    return render(request, 'blog/datetime.html', {'now': now })
+
+
+"""""
+def today_is(request):
+    now = datetime.datetime.now()
+    t = template.loader.get_template('blog/datetime.html')
+    #In modern versions of Django, the use of the Context class is not necessary. 
+    #Instead, you can pass a dictionary directly to the template rendering function.
+    c = {'now': now} 
+    html = t.render(c)
     return HttpResponse(html)
+"""
