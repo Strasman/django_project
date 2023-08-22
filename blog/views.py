9b45dd21be73226a6d675f5a7ecf57dd918427d6
Strasman <elad.strasman@gmail.com>
2023-08-22 13:42:43 +0300

    Showing 404 Errors

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.conf import settings
 from .models import Author, Tag, Category, Post
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 def index(request):
     return HttpResponse("Hello Django")
@@ -10,32 +11,31 @@ def index(request):
 
 # view function to display a list of posts
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by("-id").all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 # view function to display a single post
-def post_detail(request, pk):    
-    post = Post.objects.get(pk=pk)
+def post_detail(request, pk, post_slug):    
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
 # view function to display post by category
 def post_by_category(request, category_slug):
-    category = Category.objects.get(slug=category_slug)
-    posts = Post.objects.filter(category__slug=category_slug)
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = get_list_or_404(Post.objects.order_by("-id"), category=category)
     context = {
         'category': category,
         'posts': posts
     }
-    print(category)
     return render(request, 'blog/post_by_category.html', context)
 
 
 # view function to display post by tag
 def post_by_tag(request, tag_slug):
-    tag = Tag.objects.get(slug=tag_slug)
-    posts = Post.objects.filter(tags__name=tag)
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = get_list_or_404(Post.objects.order_by("-id"), tags=tag)
     context = {
         'tag': tag,
         'posts': posts
