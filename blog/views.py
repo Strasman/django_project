8a66c99f99b250726072c79be460000c37da51a9
Strasman <elad.strasman@gmail.com>
2023-09-11 15:21:41 +0300

    Pagination in Django

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,8 @@ from django.http import (HttpResponse, HttpResponseNotFound,
 from django.contrib import messages
 from .forms import FeedbackForm
 from django.core.mail import mail_admins
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django_project import helpers
 
 def index(request):
     return HttpResponse("Hello Django")
@@ -23,6 +25,7 @@ def test_redirect(request):
 # view function to display a list of posts
 def post_list(request):
     posts = Post.objects.order_by("-id").all()
+    posts = helpers.pg_records(request, posts, 5)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -36,6 +39,7 @@ def post_detail(request, pk, post_slug):
 def post_by_category(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     posts = get_list_or_404(Post.objects.order_by("-id"), category=category)
+    posts = helpers.pg_records(request, posts, 5)
     context = {
         'category': category,
         'posts': posts
@@ -47,6 +51,7 @@ def post_by_category(request, category_slug):
 def post_by_tag(request, tag_slug):
     tag = get_object_or_404(Tag, slug=tag_slug)
     posts = get_list_or_404(Post.objects.order_by("-id"), tags=tag)
+    posts = helpers.pg_records(request, posts, 5)
     context = {
         'tag': tag,
         'posts': posts
